619e6036e10adce7fbac87964c5a02eeb1487d37
Oscar <oscar.cortes@upf.edu>
2019-09-30 18:37:11 +0200

    Primer commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,15 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  console.log('Hello, UOC!');
+} )();
